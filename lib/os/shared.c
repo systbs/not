111799f70f7284d4dc3692e74c66535929565f9a
systbs <Yasser-Sajjadi@outlook.com>
2024-06-27 18:53:20 +0330

    standard libraries

--- lib/os/shared.c
+++ lib/os/shared.c
@@ -113,8 +113,134 @@ struct permission permissions[21] = {
     {P_S_IXOTH, S_IXOTH},
     {P_S_IRWXO, S_IRWXO}};
 
-not_record_t *
-f_open(not_record_t *args) // (const char *path, mpz_t *flag, mpz_t *mode)
+struct error
+{
+    int no;
+    int value;
+};
+
+struct error errors[97] = {
+    {EDEADLK, 35},      /* Resource deadlock would occur */
+    {ENAMETOOLONG, 36}, /* File name too long */
+    {ENOLCK, 37},       /* No record locks available */
+
+    {ENOSYS, 38}, /* Invalid system call number */
+
+    {ENOTEMPTY, 39}, /* Directory not empty */
+    {ELOOP, 40},     /* Too many symbolic links encountered */
+    {ENOMSG, 42},    /* No message of desired type */
+    {EIDRM, 43},     /* Identifier removed */
+    {ECHRNG, 44},    /* Channel number out of range */
+    {EL2NSYNC, 45},  /* Level 2 not synchronized */
+    {EL3HLT, 46},    /* Level 3 halted */
+    {EL3RST, 47},    /* Level 3 reset */
+    {ELNRNG, 48},    /* Link number out of range */
+    {EUNATCH, 49},   /* Protocol driver not attached */
+    {ENOCSI, 50},    /* No CSI structure available */
+    {EL2HLT, 51},    /* Level 2 halted */
+    {EBADE, 52},     /* Invalid exchange */
+    {EBADR, 53},     /* Invalid request descriptor */
+    {EXFULL, 54},    /* Exchange full */
+    {ENOANO, 55},    /* No anode */
+    {EBADRQC, 56},   /* Invalid request code */
+    {EBADSLT, 57},   /* Invalid slot */
+
+    {EBFONT, 59},          /* Bad font file format */
+    {ENOSTR, 60},          /* Device not a stream */
+    {ENODATA, 61},         /* No data available */
+    {ETIME, 62},           /* Timer expired */
+    {ENOSR, 63},           /* Out of streams resources */
+    {ENONET, 64},          /* Machine is not on the network */
+    {ENOPKG, 65},          /* Package not installed */
+    {EREMOTE, 66},         /* Object is remote */
+    {ENOLINK, 67},         /* Link has been severed */
+    {EADV, 68},            /* Advertise error */
+    {ESRMNT, 69},          /* Srmount error */
+    {ECOMM, 70},           /* Communication error on send */
+    {EPROTO, 71},          /* Protocol error */
+    {EMULTIHOP, 72},       /* Multihop attempted */
+    {EDOTDOT, 73},         /* RFS specific error */
+    {EBADMSG, 74},         /* Not a data message */
+    {EOVERFLOW, 75},       /* Value too large for defined data type */
+    {ENOTUNIQ, 76},        /* Name not unique on network */
+    {EBADFD, 77},          /* File descriptor in bad state */
+    {EREMCHG, 78},         /* Remote address changed */
+    {ELIBACC, 79},         /* Can not access a needed shared library */
+    {ELIBBAD, 80},         /* Accessing a corrupted shared library */
+    {ELIBSCN, 81},         /* .lib section in a.out corrupted */
+    {ELIBMAX, 82},         /* Attempting to link in too many shared libraries */
+    {ELIBEXEC, 83},        /* Cannot exec a shared library directly */
+    {EILSEQ, 84},          /* Illegal byte sequence */
+    {ERESTART, 85},        /* Interrupted system call should be restarted */
+    {ESTRPIPE, 86},        /* Streams pipe error */
+    {EUSERS, 87},          /* Too many users */
+    {ENOTSOCK, 88},        /* Socket operation on non-socket */
+    {EDESTADDRREQ, 89},    /* Destination address required */
+    {EMSGSIZE, 90},        /* Message too long */
+    {EPROTOTYPE, 91},      /* Protocol wrong type for socket */
+    {ENOPROTOOPT, 92},     /* Protocol not available */
+    {EPROTONOSUPPORT, 93}, /* Protocol not supported */
+    {ESOCKTNOSUPPORT, 94}, /* Socket type not supported */
+    {EOPNOTSUPP, 95},      /* Operation not supported on transport endpoint */
+    {EPFNOSUPPORT, 96},    /* Protocol family not supported */
+    {EAFNOSUPPORT, 97},    /* Address family not supported by protocol */
+    {EADDRINUSE, 98},      /* Address already in use */
+    {EADDRNOTAVAIL, 99},   /* Cannot assign requested address */
+    {ENETDOWN, 100},       /* Network is down */
+    {ENETUNREACH, 101},    /* Network is unreachable */
+    {ENETRESET, 102},      /* Network dropped connection because of reset */
+    {ECONNABORTED, 103},   /* Software caused connection abort */
+    {ECONNRESET, 104},     /* Connection reset by peer */
+    {ENOBUFS, 105},        /* No buffer space available */
+    {EISCONN, 106},        /* Transport endpoint is already connected */
+    {ENOTCONN, 107},       /* Transport endpoint is not connected */
+    {ESHUTDOWN, 108},      /* Cannot send after transport endpoint shutdown */
+    {ETOOMANYREFS, 109},   /* Too many references: cannot splice */
+    {ETIMEDOUT, 110},      /* Connection timed out */
+    {ECONNREFUSED, 111},   /* Connection refused */
+    {EHOSTDOWN, 112},      /* Host is down */
+    {EHOSTUNREACH, 113},   /* No route to host */
+    {EALREADY, 114},       /* Operation already in progress */
+    {EINPROGRESS, 115},    /* Operation now in progress */
+    {ESTALE, 116},         /* Stale file handle */
+    {EUCLEAN, 117},        /* Structure needs cleaning */
+    {ENOTNAM, 118},        /* Not a XENIX named type file */
+    {ENAVAIL, 119},        /* No XENIX semaphores available */
+    {EISNAM, 120},         /* Is a named type file */
+    {EREMOTEIO, 121},      /* Remote I/O error */
+    {EDQUOT, 122},         /* Quota exceeded */
+
+    {ENOMEDIUM, 123},    /* No medium found */
+    {EMEDIUMTYPE, 124},  /* Wrong medium type */
+    {ECANCELED, 125},    /* Operation Canceled */
+    {ENOKEY, 126},       /* Required key not available */
+    {EKEYEXPIRED, 127},  /* Key has expired */
+    {EKEYREVOKED, 128},  /* Key has been revoked */
+    {EKEYREJECTED, 129}, /* Key was rejected by service */
+
+    /* for robust mutexes */
+    {EOWNERDEAD, 130},      /* Owner died */
+    {ENOTRECOVERABLE, 131}, /* State not recoverable */
+
+    {ERFKILL, 132}, /* Operation not possible due to RF-kill */
+
+    {EHWPOISON, 133} /* Memory page has hardware error */
+};
+
+int get_error_no(int key)
+{
+    size_t num_errors = sizeof(errors) / sizeof(errors[0]);
+
+    for (size_t i = 0; i < num_errors; i++)
+    {
+        if (errors[i].no == key)
+            return errors[i].value;
+    }
+
+    return key;
+}
+
+not_record_t *f_open(not_record_t *args) // (const char *path, mpz_t *flag, mpz_t *mode)
 {
     not_record_t *arg0 = not_record_tuple_arg_by_index(args, 0);
     not_record_t *arg1 = not_record_tuple_arg_by_index(args, 1);
@@ -158,7 +284,7 @@ f_open(not_record_t *args) // (const char *path, mpz_t *flag, mpz_t *mode)
 
     return not_record_make_int_from_si(fd);
 err:
-    return not_record_make_int_from_si(errno);
+    return not_record_make_int_from_si(get_error_no(errno));
 }
 
 not_record_t *
@@ -210,6 +336,12 @@ f_read(not_record_t *args) // (mpz_t *fd, mpz_t *count)
         }
 
         not_record_t *record = not_record_make_string(content);
+        if (record == NOT_PTR_ERROR)
+        {
+            free(content);
+            goto err;
+        }
+
         free(content);
         return record;
     }
@@ -235,12 +367,18 @@ f_read(not_record_t *args) // (mpz_t *fd, mpz_t *count)
         }
 
         not_record_t *record = not_record_make_string(buffer);
+        if (record == NOT_PTR_ERROR)
+        {
+            free(buffer);
+            goto err;
+        }
+
         free(buffer);
         return record;
     }
 
 err:
-    return not_record_make_int_from_si(errno);
+    return not_record_make_int_from_si(get_error_no(errno));
 }
 
 static inline void
@@ -297,7 +435,7 @@ f_write(not_record_t *args) // (mpz_t *fd, const char *buf, mpz_t *n)
     char *modified = calloc(strlen(buf) + 1, sizeof(char));
     if (!modified)
     {
-        mpz_set_si(*(mpz_t *)arg3->value, ENOMEM);
+        mpz_set_si(*(mpz_t *)arg3->value, get_error_no(ENOMEM));
         goto final;
     }
     replace_escaped(buf, modified);
@@ -305,7 +443,7 @@ f_write(not_record_t *args) // (mpz_t *fd, const char *buf, mpz_t *n)
     t = write(fd, modified, nbytes);
     if (t < 0)
     {
-        mpz_set_si(*(mpz_t *)arg3->value, errno);
+        mpz_set_si(*(mpz_t *)arg3->value, get_error_no(errno));
     }
 
     free(modified);
@@ -327,7 +465,7 @@ f_close(not_record_t *args) // (mpz_t *fd)
     }
     return not_record_make_int_from_si(t);
 err:
-    return not_record_make_int_from_si(errno);
+    return not_record_make_int_from_si(get_error_no(errno));
 }
 
 #define P_SEEK_SET 0
@@ -424,9 +562,9 @@ f_gets(not_record_t *args)
 {
     size_t buffer_size = 100;
     char *buffer = (char *)malloc(buffer_size * sizeof(char));
-
     if (buffer == NULL)
     {
+        errno = ENOMEM;
         goto err;
     }
 
@@ -440,11 +578,16 @@ f_gets(not_record_t *args)
     }
 
     not_record_t *record = not_record_make_string(buffer);
+    if (record == NOT_PTR_ERROR)
+    {
+        free(buffer);
+        goto err;
+    }
     free(buffer);
     return record;
 
 err:
-    return not_record_make_null();
+    return not_record_make_int_from_si(get_error_no(errno));
 }
 
 not_record_t *
@@ -455,7 +598,7 @@ f_getc(not_record_t *args)
     return not_record_make_char(c);
 
 err:
-    return not_record_make_null();
+    return not_record_make_int_from_si(get_error_no(errno));
 }
 
 not_record_t *
@@ -467,7 +610,7 @@ f_stat(not_record_t *args) // (const char *path)
     struct stat st;
     if (stat(path, &st) < 0)
     {
-        return not_record_make_int_from_si(errno);
+        goto err;
     }
 
     size_t num_permissions = sizeof(permissions) / sizeof(permissions[0]);
@@ -621,7 +764,7 @@ cleanup:
         not_record_object_destroy(top);
 
 err:
-    return not_record_make_null();
+    return not_record_make_int_from_si(get_error_no(errno));
 }
 
 not_record_t *
@@ -636,7 +779,7 @@ f_remove(not_record_t *args) // (const char *path)
     }
     else
     {
-        return not_record_make_int_from_si(errno);
+        return not_record_make_int_from_si(get_error_no(errno));
     }
 }
 
@@ -661,7 +804,7 @@ d_read(not_record_t *args) // (const char *path)
     DIR *dp = opendir(path);
     if (dp == NULL)
     {
-        return not_record_make_int_from_si(errno);
+        return not_record_make_int_from_si(get_error_no(errno));
     }
 
     not_record_tuple_t *tuple_top = NULL,
@@ -777,7 +920,7 @@ err:
     if (tuple_top)
         not_record_tuple_destroy(tuple_top);
 
-    return not_record_make_int_from_si(errno);
+    return not_record_make_int_from_si(get_error_no(errno));
 }
 
 not_record_t *
@@ -792,7 +935,7 @@ d_remove(not_record_t *args) // (const char *path)
     }
     else
     {
-        return not_record_make_int_from_si(errno);
+        return not_record_make_int_from_si(get_error_no(errno));
     }
 }
 
@@ -820,6 +963,6 @@ d_make(not_record_t *args) // (const char *path)
     }
     else
     {
-        return not_record_make_int_from_si(errno);
+        return not_record_make_int_from_si(get_error_no(errno));
     }
 }
